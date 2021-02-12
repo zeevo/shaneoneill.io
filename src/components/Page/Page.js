42d7537bc63188fbe7718d8c375d398660549113
zeevosec <8338013+zeevosec@users.noreply.github.com>
2021-02-11 20:46:19 -0500

    Use gatsby image

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,21 +1,13 @@
 import React, { useRef, useEffect } from 'react';
 import styles from './Page.module.scss';
 
-const Page = ({ title, children }) => {
-  const pageRef = useRef();
-
-  useEffect(() => {
-    pageRef.current.scrollIntoView();
-  });
-
-  return (
-    <div ref={pageRef} className={styles['page']}>
-      <div className={styles['page__inner']}>
-        {title && <h1 className={styles['page__title']}>{title}</h1>}
-        <div className={styles['page__body']}>{children}</div>
-      </div>
+const Page = ({ title, children }) => (
+  <div className={styles['page']}>
+    <div className={styles['page__inner']}>
+      {title && <h1 className={styles['page__title']}>{title}</h1>}
+      <div className={styles['page__body']}>{children}</div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Page;
--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -1,48 +1,59 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import React from 'react';
-import { withPrefix, Link } from 'gatsby';
+import Img from 'gatsby-image';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import styles from './Author.module.scss';
 
-const Author = ({ author, isIndex }) => (
-  <div className={styles['author']}>
-    <Link to="/">
-      <img
-        src={withPrefix(author.photo)}
-        className={styles['author__photo']}
-        width="75"
-        height="75"
-        alt={author.name}
-      />
-    </Link>
+const Author = ({ author, isIndex }) => {
+  const { file } = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "photo.png" }) {
+        childImageSharp {
+          # Specify the image processing specifications right in the query.
+          # Makes it trivial to update as your page's design changes.
+          fixed(width: 125, height: 125) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
 
-    <div className={styles['author__block']}>
-      {isIndex === true ? (
-        <h1 className={styles['author__title']}>
-          <Link className={styles['author__title-link']} to="/" sx={{ textDecoration: 'none' }}>
-            {author.name}
-          </Link>
-        </h1>
-      ) : (
-        <h2 className={styles['author__title']}>
-          <Link className={styles['author__title-link']} to="/" sx={{ textDecoration: 'none' }}>
-            {author.name}
-          </Link>
-        </h2>
-      )}
-      <h6 className={styles['author__subtitle']}>
-        {author.bio
-          .split('.')
-          .filter((ele) => ele)
-          .map((ele) => (
-            <>
-              <span>{ele}.</span>
-              <br />
-            </>
-          ))}
-      </h6>
+  return (
+    <div className={styles['author']}>
+      <Link to="/">
+        <Img fixed={file.childImageSharp.fixed} alt={author.name} />
+      </Link>
+
+      <div className={styles['author__block']}>
+        {isIndex === true ? (
+          <h1 className={styles['author__title']}>
+            <Link className={styles['author__title-link']} to="/" sx={{ textDecoration: 'none' }}>
+              {author.name}
+            </Link>
+          </h1>
+        ) : (
+          <h2 className={styles['author__title']}>
+            <Link className={styles['author__title-link']} to="/" sx={{ textDecoration: 'none' }}>
+              {author.name}
+            </Link>
+          </h2>
+        )}
+        <h6 className={styles['author__subtitle']}>
+          {author.bio
+            .split('.')
+            .filter((ele) => ele)
+            .map((ele) => (
+              <>
+                <span>{ele}.</span>
+                <br />
+              </>
+            ))}
+        </h6>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Author;
